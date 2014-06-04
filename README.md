43acb4a10773437472898eb1c2c9825e0f3140da
Balakrishna V <balakrishna.v@imaginea.com>
2014-06-04 15:57:41 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-tomcat-monitoring-extension
-===========================
+# AppDynamics Tomcat Monitoring Extension
 
-AppDynamics Tomcat Monitoring Extension
+This extension works only with the standalone machine agent.
+
+##Use Case
+
+Apache Tomcat is an open source software implementation of the Java Servlet and JavaServer Pages technologies. This eXtension monitors Tomcat instance and collects useful statistics exposed through MBeans and reports to AppDynamics Controller. JMX has to be enabled in the Tomcat instance for this extension to work. To enable JMX please refer [here] (http://tomcat.apache.org/tomcat-7.0-doc/monitoring.html).
+
+##Installation
+
+##Metrics
+
+## Custom Dashboard
+![]()
+
+##Contributing
+
+Always feel free to fork and contribute any changes directly here on GitHub.
+
+##Community
+
+Find out more in the [AppSphere]() community.
+
+##Support
+
+For any questions or feature request, please contact [AppDynamics Center of Excellence](mailto:ace-request@appdynamics.com).
---
+++ src/main/java/com/appdynamics/extensions/tomcat/TomcatMonitor.java
@@ -0,0 +1,185 @@
+/**
+ * Copyright 2014 AppDynamics, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.appdynamics.extensions.tomcat;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import javax.management.MBeanAttributeInfo;
+import javax.management.ObjectInstance;
+import javax.management.ObjectName;
+import javax.management.remote.JMXConnector;
+
+import org.apache.log4j.Logger;
+
+import com.appdynamics.extensions.PathResolver;
+import com.appdynamics.extensions.jmx.JMXConnectionConfig;
+import com.appdynamics.extensions.jmx.JMXConnectionUtil;
+import com.appdynamics.extensions.jmx.MBeanKeyPropertyEnum;
+import com.appdynamics.extensions.tomcat.config.ConfigUtil;
+import com.appdynamics.extensions.tomcat.config.Configuration;
+import com.appdynamics.extensions.tomcat.config.MBeans;
+import com.appdynamics.extensions.tomcat.config.Server;
+import com.google.common.base.Strings;
+import com.singularity.ee.agent.systemagent.api.AManagedMonitor;
+import com.singularity.ee.agent.systemagent.api.MetricWriter;
+import com.singularity.ee.agent.systemagent.api.TaskExecutionContext;
+import com.singularity.ee.agent.systemagent.api.TaskOutput;
+import com.singularity.ee.agent.systemagent.api.exception.TaskExecutionException;
+
+public class TomcatMonitor extends AManagedMonitor {
+
+	public static final Logger logger = Logger.getLogger(TomcatMonitor.class);
+	public static final String METRICS_SEPARATOR = "|";
+	private static final String CONFIG_ARG = "config-file";
+
+	private static final ConfigUtil<Configuration> configUtil = new ConfigUtil<Configuration>();
+
+	private JMXConnectionUtil jmxConnector;
+
+	public TomcatMonitor() {
+		String msg = "Using Monitor Version [" + getImplementationVersion() + "]";
+		logger.info(msg);
+		System.out.println(msg);
+	}
+
+	public TaskOutput execute(Map<String, String> taskArgs, TaskExecutionContext arg1) throws TaskExecutionException {
+		if (taskArgs != null) {
+			logger.info("Starting the Tomcat Monitoring task.");
+			String configFilename = getConfigFilename(taskArgs.get(CONFIG_ARG));
+			try {
+				Configuration config = configUtil.readConfig(configFilename, Configuration.class);
+				Map<String, String> metrics = populateMetrics(config);
+				printStats(config, metrics);
+				logger.info("Completed the Tomcat Monitoring Task successfully");
+				return new TaskOutput("Tomcat Monitor executed successfully");
+			} catch (FileNotFoundException e) {
+				logger.error("Config File not found: " + configFilename, e);
+			} catch (Exception e) {
+				logger.error("Metrics Collection Failed: ", e);
+			}
+		}
+		throw new TaskExecutionException("Tomcat Monitor completed with failures");
+	}
+
+	private Map<String, String> populateMetrics(Configuration config) throws Exception {
+		Map<String, String> metrics = new HashMap<String, String>();
+		Server server = config.getServer();
+		MBeans mbeanData = config.getMbeans();
+		jmxConnector = new JMXConnectionUtil(new JMXConnectionConfig(server.getHost(), server.getPort(), server.getUsername(), server.getPassword()));
+		JMXConnector connector = jmxConnector.connect();
+		if (connector != null) {
+			Set<ObjectInstance> allMbeans = jmxConnector.getAllMBeans();
+			if (allMbeans != null) {
+				metrics = extractMetrics(mbeanData, allMbeans);
+			}
+		}
+		return metrics;
+	}
+
+	private Map<String, String> extractMetrics(MBeans mbeanData, Set<ObjectInstance> allMbeans) {
+		Map<String, String> metrics = new HashMap<String, String>();
+		for (ObjectInstance mbean : allMbeans) {
+			ObjectName objectName = mbean.getObjectName();
+			if (isDomainAndKeyPropertyConfigured(objectName, mbeanData)) {
+				MBeanAttributeInfo[] attributes = jmxConnector.fetchAllAttributesForMbean(objectName);
+				if (attributes != null) {
+					for (MBeanAttributeInfo attr : attributes) {
+						if (attr.isReadable()) {
+							Object attribute = jmxConnector.getMBeanAttribute(objectName, attr.getName());
+							if (attribute != null && attribute instanceof Number) {
+								String metricKey = getMetricsKey(objectName, attr);
+								metrics.put(metricKey, attribute.toString());
+							}
+						}
+					}
+				}
+			}
+		}
+		return metrics;
+	}
+
+	private boolean isDomainAndKeyPropertyConfigured(ObjectName objectName, MBeans mbeanData) {
+		String domain = objectName.getDomain();
+		String keyProperty = objectName.getKeyProperty(MBeanKeyPropertyEnum.TYPE.toString());
+		Set<String> types = mbeanData.getTypes();
+		boolean configured = mbeanData.getDomainName().equals(domain) && types.contains(keyProperty);
+		return configured;
+	}
+
+	private String getMetricsKey(ObjectName objectName, MBeanAttributeInfo attr) {
+		String name = objectName.getKeyProperty(MBeanKeyPropertyEnum.NAME.toString());
+		String type = objectName.getKeyProperty(MBeanKeyPropertyEnum.TYPE.toString());
+		StringBuilder metricsKey = new StringBuilder();
+		metricsKey.append(Strings.isNullOrEmpty(name) ? "" : name + METRICS_SEPARATOR);
+		metricsKey.append(Strings.isNullOrEmpty(type) ? "" : type + METRICS_SEPARATOR);
+		metricsKey.append(attr.getName());
+
+		return metricsKey.toString();
+	}
+
+	private void printStats(Configuration config, Map<String, String> metrics) {
+		String metricPath = config.getMetricPrefix();
+		for (Map.Entry<String, String> entry : metrics.entrySet()) {
+			printMetric(metricPath + entry.getKey(), entry.getValue());
+		}
+	}
+
+	/**
+	 * Returns a config file name,
+	 * 
+	 * @param filename
+	 * @return String
+	 */
+	private String getConfigFilename(String filename) {
+		if (filename == null) {
+			return "";
+		}
+		// for absolute paths
+		if (new File(filename).exists()) {
+			return filename;
+		}
+		// for relative paths
+		File jarPath = PathResolver.resolveDirectory(AManagedMonitor.class);
+		String configFileName = "";
+		if (!Strings.isNullOrEmpty(filename)) {
+			configFileName = jarPath + File.separator + filename;
+		}
+		return configFileName;
+	}
+
+	private void printMetric(String metricPath, String metricValue) {
+		printMetric(metricPath, metricValue, MetricWriter.METRIC_AGGREGATION_TYPE_AVERAGE, MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE,
+				MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_COLLECTIVE);
+	}
+
+	private void printMetric(String metricPath, String metricValue, String aggregation, String timeRollup, String cluster) {
+		MetricWriter metricWriter = super.getMetricWriter(metricPath, aggregation, timeRollup, cluster);
+		if (metricValue != null) {
+			if (logger.isDebugEnabled()) {
+				logger.debug("Metric [" + metricPath + " = " + metricValue + "]");
+			}
+			metricWriter.printMetric(metricValue);
+		}
+	}
+
+	private static String getImplementationVersion() {
+		return TomcatMonitor.class.getPackage().getImplementationTitle();
+	}
+}
